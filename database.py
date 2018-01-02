d7bf4c82e1dda1bbc8a1bb7842b1c56bbf9220a7
michelamar <mmarchini@stuy.edu>
2018-01-02 14:39:10 -0500

    trying to figure out what isn't working

--- database.py
+++ database.py
@@ -4,15 +4,19 @@ f = "data/database.db"
 db = sqlite3.connect(f)
 c = db.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS accounts (username TEXT PRIMARY KEY, password TEXT);')
-c.execute('CREATE TABLE IF NOT EXISTS scores (game TEXT, username TEXT, score NUMERIC);')
+c.execute('CREATE TABLE IF NOT EXISTS scores (game TEXT, username TEXT, score INTEGER);')
 c.execute('CREATE TABLE IF NOT EXISTS teams (teamname TEXT, members TEXT);')
+#db.close()
+
+c.execute('INSERT INTO scores VALUES("simon", "shanny_boy", 22);')
+
 db.close()
 
 #adds a users new score
 def add_score(game, user, score):
-    f = "data/database.db"
-    db = sqlite3.connect(f)
-    c = db.cursor()
+    #f = "data/database.db"
+    #db = sqlite3.connect(f)
+    #c = db.cursor()
     c.execute('INSERT INTO scores VALUES("%s", "%s", %d);'%(game, user, score))
     db.close()
     
@@ -33,6 +37,17 @@ def add_member(name, member):
     c = db.cursor()
     members = c.execute('SELECT members FROM teams WHERE teamname = "%s";'%name)
     members.append(member)
+    c.execute('DELETE FROM teams WHERE teamname = "%s";'%name)
+    c.execute('INSERT INTO scores VALUES("%s", "%s");'%(name, members))
+    db.close()
+
+#removes a member from a team
+def remove_member(name, member):
+    f = "data/database.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    members = c.execute('SELECT members FROM teams WHERE teamname = "%s";'%name)
+    members.remove(member)
     c.execute('DELETE FROM teams WHERE teamname = "%s";'%name)
     c.execute('INSERT INTO scores VALUES("%s", "%s");'%(name, members))
     db.close()
@@ -64,6 +79,5 @@ def get_game_highscore(game):
         db.close()
         return result[0][0]
 
-
-
+#add_score('simon', 'shanny_boy', 22)
               
