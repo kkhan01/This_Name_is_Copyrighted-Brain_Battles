f7612fd2ec3c7989db1e17c5b0ae8394d3b63936
Mansour <melsharawy@stuy.edu>
2018-01-03 14:36:40 -0500

    react timer changes

--- static/js/react.js
+++ static/js/react.js
@@ -1,11 +1,12 @@
 var box = document.getElementById("box");
+box.setAttribute("class","full");
 var body = document.getElementsByTagName("body")[0];
 var start = document.getElementById("start");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
-box.setAttribute("style","background-color:blue;");
+box.setAttribute("style","background-color:#0099CC;");
 var directions = document.createElement("h1");
-directions.setAttribute("style","position:absolute;left:40%;top:50%");
+directions.setAttribute("style","position:relative;top:50%");
 directions.innerHTML = "Click anywhere to start!";
 box.appendChild(directions);
 var activate = 0;
@@ -15,14 +16,14 @@ var timeoutevent;
 
 
 var startup = function(){
-    box.setAttribute("style","background-color:red;");
+    box.setAttribute("style","background-color:#ff3f3f;");
     directions.innerHTML = "Wait for it...";
     needclick = 0;
     timeoutevent = setTimeout(go,randTime);
 };
 
 var go = function(){
-    box.setAttribute("style","background-color:green;");
+    box.setAttribute("style","background-color:#43f961;");
     directions.innerHTML = "Click!";
     needclick = 1;
     var date = new Date();
@@ -33,14 +34,14 @@ var onClick = function(e){
     if(needclick == 1){
 	var newdate = new Date();
 	var clicktime = newdate.getTime();
-	this.setAttribute("style","background-color:blue;");
+	this.setAttribute("style","background-color:#0099CC;");
 	var reacttime = (clicktime - activate);
 	directions.innerHTML = "" + reacttime + " ms";
 	needclick = 2;
     }
     
     else if(needclick == 0){
-	this.setAttribute("style","background-color:blue;");
+	this.setAttribute("style","background-color:#0099CC;");
 	directions.innerHTML = "Too soon, try again!";
 	clearTimeout(timeoutevent);
 	needclick = 2;
