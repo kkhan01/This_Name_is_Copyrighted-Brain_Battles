703901f674cfd6d1d0ec1230fac7d6a2130388ab
Mansour <melsharawy@stuy.edu>
2018-01-09 14:45:18 -0500

    ajax for wordsearch done

--- static/js/wordsearch.js
+++ static/js/wordsearch.js
@@ -1,16 +1,28 @@
-var transmit = function( e ) {
+var wordlist = [];
+var transmit = function() {
 
   $.ajax({
-    url: 'http://www.randomtext.me/api/gibberish/p-1/30',
+    url: 'http://www.randomtext.me/api/gibberish/p-1/100',
     type: 'GET',
-    data: {'text' : input },
     success: function(d) {
-      console.log(d);
-      console.log(JSON.parse(d));
+	var randomwords = d["text_out"].replace("<p>","").replace("</p>","");
+	//console.log(randomwords);
+	construct_list(randomwords,10,4,8);
     } //end success callback
   });//end ajax call
-  console.log('goodbye');
 }; //end transmit function
 
-transmit(e);
+var construct_list = function (s,num,min,max){
+    var list = s.split(" ");
+    //console.log(list);
+    for (i in list){
+	//console.log(list[i]);
+	if(wordlist.length < num && list[i].length <= max && list[i].length >=min && wordlist.indexOf(list[i]) == -1){
+	    wordlist.push(list[i]);
+	}
+    }
+    console.log(wordlist);
+};
+
+transmit();
 //document.getElementById("input").addEventListener('input', transmit);
