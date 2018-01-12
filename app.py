25dcffa17ef7baf1823e4df37f2c7128a5205332
Khinshan Khan <kkhan@stuy.edu>
2018-01-12 15:34:31 -0500

    disabled browser caching? image updating looks fine

--- app.py
+++ app.py
@@ -143,6 +143,18 @@ def get_game_highscore(game):
 #==========================================================
 #flask code
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/')                                                                 
 def root():
     if 'user' in session:
