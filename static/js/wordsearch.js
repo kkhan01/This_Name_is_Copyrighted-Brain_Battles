5d75df5fd81273a12c6e9be16fdda74c86df78b3
Stanley Lin <slin55055@gmail.com>
2018-01-13 20:35:05 -0500

    working on wordsearch

--- static/js/wordsearch.js
+++ static/js/wordsearch.js
@@ -1,28 +1,101 @@
-var wordlist = [];
-var transmit = function() {
+"use strict";
+
+let wordlist, grid;
+
+const GRID_LEN = 6;
+const DIRECTIONS = {
+	NORTH: [0, -1],
+	NORTHEAST: [1, -1],
+	EAST: [1, 0],
+	SOUTHEAST: [1, 1],
+	SOUTH: [0, 1],
+	SOUTHWEST: [-1, 1],
+	WEST: [-1, 0],
+	NORTHWEST: [-1, -1]
+};
+Object.freeze(DIRECTIONS);
+for (let d in DIRECTIONS) Object.freeze(DIRECTIONS[d]);	//effectively an enum now
+
+function transmit() {
 
   $.ajax({
     url: 'http://www.randomtext.me/api/gibberish/p-1/100',
     type: 'GET',
     success: function(d) {
-	var randomwords = d["text_out"].replace("<p>","").replace("</p>","").toUpperCase();
+	let randomwords = d["text_out"].replace("<p>","").replace("</p>","").toUpperCase();
 	//console.log(randomwords);
-	construct_list(randomwords,10,4,8);
+	constructList(randomwords,10,4,8);
     } //end success callback
   });//end ajax call
 }; //end transmit function
 
-var construct_list = function (s,num,min,max){
-    var list = s.split(" ");
+function constructList(s, num, min, max) {
+    let temp = s.split(" ");
     //console.log(list);
-    for (i in list){
+    for (let i in temp){
 	//console.log(list[i]);
-	if(wordlist.length < num && list[i].length <= max && list[i].length >=min && wordlist.indexOf(list[i]) == -1){
-	    wordlist.push(list[i]);
+	if(wordlist.length < num && temp[i].length <= max && temp[i].length >=min && wordlist.indexOf(temp[i]) == -1){
+	    wordlist.push(temp[i]);
 	}
     }
     console.log(wordlist);
 };
 
-transmit();
+function init() {
+	wordlist = [];
+	grid = [];
+	
+	for (let y = 0; y < GRID_LEN; y++) {
+		grid.push([]);
+		for (let x = 0; x < GRID_LEN; x++) {
+			grid[y].push("-");
+		}
+		
+	}
+	
+	console.log(grid);
+}
+
+function addSingleWord(word, row, col, direction){
+	let counter;
+	let tRow, tCol; 
+	let runs = word;
+	for (	let turn = 0;
+			turn < 2;
+			runs = new StringBuilder(word).reverse().toString(), turn++) {
+		if ((mData[row].length - col) < runs.length()) continue;
+	
+			try {
+				tRow = row;
+				tCol = col;
+				counter = 0;
+				for (counter = 0; counter < runs.length(); counter++) {
+					if (mData[tRow][tCol] != '_' && 
+						mData[tRow][tCol] != runs.charAt(counter)) {
+						break;
+					}
+					tRow += direction.getDeltaY();
+					tCol += direction.getDeltaX();
+				}
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				return false;
+			}
+	
+		if (counter == runs.length()) {
+			for (counter = 0; counter < runs.length(); counter++) {
+				mData[row][col] = runs.charAt(counter);
+				col += direction.getDeltaX();
+					row += direction.getDeltaY();
+			}
+			return true;
+		}
+		else continue;
+	}
+	return false;
+}
+
+
+init();
+//transmit();
 //document.getElementById("input").addEventListener('input', transmit);
